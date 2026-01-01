50fe4e0e4ac767c092da0c6300ce86309e54ce09
aru-g2004 <guptaarushi68@gmail.com>
2026-01-01 00:49:18 -0500

    env

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()],
     define: {
-      'process.env.API_KEY': JSON.stringify("AIzaSyAHSxAuYde4mz4SQGV5n55npOFxv42e7_c"),
-      'process.env.GEMINI_API_KEY': JSON.stringify("AIzaSyAHSxAuYde4mz4SQGV5n55npOFxv42e7_c")
+      'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
     },
     resolve: {
       alias: {
